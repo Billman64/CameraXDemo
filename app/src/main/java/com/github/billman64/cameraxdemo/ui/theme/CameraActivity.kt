f12edb46bd02cb90f1f4829959a0493b13913276
Bill <striderbillman@gmail.com>
2023-10-27 19:45:12 -0400

    Creaated PreviewView and ProcessCameraProvider.

--- app/src/main/java/com/github/billman64/cameraxdemo/ui/theme/CameraActivity.kt
+++ app/src/main/java/com/github/billman64/cameraxdemo/ui/theme/CameraActivity.kt
@@ -3,23 +3,61 @@ package com.github.billman64.cameraxdemo.ui.theme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import com.github.billman64.cameraxdemo.R
 import com.google.common.util.concurrent.ListenableFuture
 
 class CameraActivity : ComponentActivity() {
     val TAG = "CameraActivity" // this.localClassName
-    lateinit var previewView:PreviewView
-    private lateinit var cameraProviderFuture:ListenableFuture<ProcessCameraProvider>
-    private lateinit var textView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
         Log.d(TAG, "onCreate()")
+        var previewView:PreviewView = findViewById(R.id.previewView)
+        var cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        var textView:TextView = findViewById(R.id.orientation)
+
+        cameraProviderFuture.addListener(
+            {
+                run {
+                    try{
+                        var cameraProvider = ProcessCameraProvider.getInstance(this)
+                        bindImageAnalysis(cameraProvider)
+
+
+                    } catch (e:Exception){
+                        e.printStackTrace()
+                    }
+                }
+            }, ContextCompat.getMainExecutor(this)
+        )
+
+
+
+    }
+
+    private fun bindImageAnalysis(cameraProvider: ListenableFuture<ProcessCameraProvider>) {
+
+//        var imageAnalysis: ImageAnalysis.Builder = ImageAnalysis.Builder().setTargetResolution(Size(1280, 720))
+//            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+//            .build()
+//            .setAnalyzer(ContextCompat.getMainExecutor(this), ImageAnalysis.Analyzer {
+//                analyze(image:ImageProxy){
+//
+//                }
+//
+//            }
+//            )
+
+
     }
 }
