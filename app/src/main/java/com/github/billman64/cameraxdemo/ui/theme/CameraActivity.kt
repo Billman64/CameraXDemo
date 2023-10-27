6f6591580ac2837a4cf0b5b25f463c88062739d2
Bill <striderbillman@gmail.com>
2023-10-27 18:46:28 -0400

    Camera permissions check and request.

--- app/src/main/java/com/github/billman64/cameraxdemo/ui/theme/CameraActivity.kt
+++ app/src/main/java/com/github/billman64/cameraxdemo/ui/theme/CameraActivity.kt
@@ -2,13 +2,16 @@ package com.github.billman64.cameraxdemo.ui.theme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.activity.ComponentActivity
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import com.github.billman64.cameraxdemo.R
 import com.google.common.util.concurrent.ListenableFuture
 
-class CameraActivity : AppCompatActivity() {
+class CameraActivity : ComponentActivity() {
+    val TAG = "CameraActivity" // this.localClassName
     lateinit var previewView:PreviewView
     private lateinit var cameraProviderFuture:ListenableFuture<ProcessCameraProvider>
     private lateinit var textView: TextView
@@ -16,5 +19,7 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        Log.d(TAG, "onCreate()")
     }
 }
